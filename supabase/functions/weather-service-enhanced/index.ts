029e00eb2262250394a85ea87afec8006ea2e496
Katja Hollaar <afkatja@gmail.com>
2025-10-23 21:18:16 -0600

    chore: use cors

--- supabase/functions/weather-service-enhanced/index.ts
+++ supabase/functions/weather-service-enhanced/index.ts
@@ -1,16 +1,7 @@
 import costaRicaDestinations from "../../../src/lib/shared/destinations.ts"
-import calculateDistance from "../_shared/calculateDistance.ts"
+import { corsHeaders } from "../_shared/cors.ts"
 
 Deno.serve(async req => {
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers":
-      "authorization, x-client-info, apikey, content-type",
-    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, PATCH",
-    "Access-Control-Max-Age": "86400",
-    "Access-Control-Allow-Credentials": "false",
-  }
-
   if (req.method === "OPTIONS") {
     return new Response(null, { status: 200, headers: corsHeaders })
   }
